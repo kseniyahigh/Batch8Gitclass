d6a79fae1d2d6a1f7169ca5dd74e781f8a0858db
kseniyahigh <kseniyahigh@gmail.com>
2021-01-27 19:13:11 -0800

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Main {
     public static void main(String[]args){
+
+
         System.out.println("hello batch 8");
+
+        System.out.println("second commit");
     }
 }
